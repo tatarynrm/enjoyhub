56ccb2ece1088312528f5faed5877e1c6439597e
tatarynrm <tatarynrm@gmail.com>
2023-11-10 00:25:17 +0200

    ewqewqweqewq

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+function App() {
+  return (
+ <>
+ <header>
+  <h1>Enjoy Hub</h1>
+ </header>
+ <main>
+<div className="about">
+  <p>Тут буде статистика</p>
+  <p>Реферальна система</p>
+</div>
+ </main>
+ <footer>
+  <h3>Enjoy Hub</h3>
+ </footer>
+ </>
+  );
+}
+
+export default App;
