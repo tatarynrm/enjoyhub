f7341c9dc897ba2bb7aabaf3239aa5d614fcdb05
tatarynrm <tatarynrm@gmail.com>
2023-11-13 17:07:04 +0200

    qeweqwewq

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 const tele = window.Telegram.WebApp
 
 function App() {
+  const [users,setUsers] = useState([])
   useEffect(()=>{
     tele.ready()
   },[])
+useEffect(()=>{
+const usersCount = async (req,res) =>{
+  try {
+    // const data = await axios.get('https://api.noris.tech/statistic/statistic-users')
+    const data = await axios.get('http://localhost:5005/statistic/statistic-users')
+    console.log(data);
+    setUsers(data.data)
+  } catch (error) {
+    console.log(error);
+  }
+}
+usersCount()
+},[])
+
   return (
  <>
  <header>
@@ -12,8 +28,11 @@ function App() {
  </header>
  <main>
 <div className="about">
-  <a>Тут буде статистика</a>
-  <a>Реферальна система</a>
+  <a style={{display:"flex",gap:"20px",alignItems:"center"}}><h3>Subscribers:</h3> <span>{users[0]?.total_users}</span></a>
+ <div className="sex" style={{display:"flex",alignItems:"center",gap:"20px",justifyContent:"center"}}>
+ <a style={{display:"flex",gap:"20px",alignItems:"center"}}><h3>Boys: </h3> <span>{users[0]?.boys_count}</span></a>
+ <a style={{display:"flex",gap:"20px",alignItems:"center"}}><h3>Girls:</h3> <span>{users[0]?.girls_count}</span></a>
+ </div>
 </div>
  </main>
  <footer>
